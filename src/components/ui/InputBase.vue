4b1d1028b53b5b39051040e38265e63f2bfe2713
DmitriiPavlyuchenko <dorshichek1996@gmail.com>
2022-10-18 18:42:06 +0300

    Features (input-base): create input base

---
+++ src/components/ui/InputBase.vue
@@ -0,0 +1,29 @@
+<template>
+  <input
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  name: 'InputBase',
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  },
+  emits: ['input']
+}
+</script>
+
+<style lang="css" module>
+input {
+  color: var(--black);
+}
+
+input::placeholder {
+  color: var(--black);
+}
+</style>
