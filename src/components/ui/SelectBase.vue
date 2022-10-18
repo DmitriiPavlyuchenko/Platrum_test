a2d44351b92f6ef07792b568862618762e4c504b
DmitriiPavlyuchenko <dorshichek1996@gmail.com>
2022-10-18 18:43:23 +0300

    Features (select-base): create modal base

---
+++ src/components/ui/SelectBase.vue
@@ -0,0 +1,31 @@
+<template>
+  <select v-if="isUsersEmpty">
+    <option value="" disabled selected>{{ defaultValue }}</option>
+    <option v-for="user in users" :key="user.name">{{ user.name }}</option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: 'SelectBase',
+  props: {
+    users: {
+      required: true,
+      type: Array
+    },
+    defaultValue: {
+      required: false,
+      type: String
+    }
+  },
+  computed: {
+    isUsersEmpty () {
+      return this.users.length > 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
