0d2044f2f3e04082508446c4467ed6db17cc2532
DmitriiPavlyuchenko <dorshichek1996@gmail.com>
2022-10-20 00:48:57 +0300

    Refactoring (app): fix naming

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,77 +1,12 @@
 <template>
-  <div :class="$style.container">
-    <div :class="$style.buttons">
-      <button-base type="button" @click="openModal" :class="$style.button">Добавить</button-base>
-    </div>
-    <table-app :users="users"/>
-    <adding-user-modal :users-storage="users" @users="addUser" @closeModal="close" :isModalOpen="toggleModal"/>
-  </div>
+  <users-list/>
 </template>
 
 <script lang="js">
-import TableApp from '../components/Table/TableApp'
-import AddingUserModal from '../components/modals/AddingUserModal'
-import {getItem, setItem} from '../helpers/local-storage'
+import UsersList from '../components/UsersList'
 
 export default {
   name: 'HomeView',
-
-  components: {TableApp, AddingUserModal},
-
-  data () {
-    return {
-      toggleModal: false,
-      users: null
-    }
-  },
-
-  created () {
-    this.getUsers()
-  },
-
-  methods: {
-    openModal () {
-      this.toggleModal = true
-    },
-
-    close () {
-      this.toggleModal = false
-    },
-
-    addUser (value) {
-      this.users.push(value)
-      setItem('users', this.users)
-    },
-
-    getUsers () {
-      const users = getItem('users')
-      if (users) {
-        this.users = users
-      }
-    }
-  }
+  components: {UsersList}
 }
 </script>
-
-<style module lang="css">
-.container {
-  display: flex;
-  flex-direction: column;
-  gap: 2.5rem;
-  width: 45%;
-  height: 100vh;
-  margin: 0 auto;
-  justify-content: center;
-  align-items: center;
-}
-
-.buttons {
-  align-self: flex-end;
-}
-
-@media (max-width: 800px) {
-  .container {
-    width: 90%;
-  }
-}
-</style>
